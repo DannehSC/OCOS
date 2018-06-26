96aedd4607803d37b98df01802aad1d15a7bd898
DannehSC <dangerspookycanyon@gmail.com>
2018-06-26 08:44:47 -0500

    Add files via upload

---
+++ downloadmanifest.lua
@@ -0,0 +1,7 @@
+--FILE = SITE
+
+return {
+	["kernel.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/kernel.lua",
+	["logger.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/logger.lua"
+	["internetHandler.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/internetHandler.lua",
+}
