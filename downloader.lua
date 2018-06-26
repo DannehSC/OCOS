4cf22edfed6bee767ea8ca03a6307dc3f73c8944
DannehSC <dangerspookycanyon@gmail.com>
2018-06-26 08:50:33 -0500

    Update downloader.lua

--- downloader.lua
+++ downloader.lua
@@ -2,14 +2,30 @@ if not component.isAvailable('internet') then
 	error('No internet available. Cannot download.')
 end
 
+local fs = require('filesystem')
 local internet = require('internet')
-local file_sys = require('filesystem')
+
+local manifestURL = "https://raw.githubusercontent.com/DannehSC/OCOS/master/downloadmanifest.lua"
+
+if not fs.exists('./downloadmanifest.lua') then
+	local manData = internet.request(manifestURL)
+	if manData then
+		local file = io.open('./downloadmanifest.lua', 'w')
+		file:write(manData)
+		file:close()
+	else
+		error('[ERROR] Unable to fetch manifest.')
+	end
+end
+
 local manifest = require('./downloadmanifest.lua')
 
 for i, v in pairs(manifest) do
 	local data = internet.request(v)
 	if data then
-		
+		local file = io.open('./' .. i, 'w')
+		file:write(data)
+		file:close()
 	else
 		print('[WARNING] Unable to request ' .. v)
 	end
