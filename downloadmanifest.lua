c177041a5b03e1552730c47678b6cd7bb6f70e44
DannehSC <dangerspookycanyon@gmail.com>
2018-06-26 09:04:32 -0500

    Update downloadmanifest.lua

--- downloadmanifest.lua
+++ downloadmanifest.lua
@@ -2,8 +2,8 @@
 
 local fileManifest = {
 	["kernel.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/kernel.lua",
-	["logger.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/logger.lua"
-	["internetHandler.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/internetHandler.lua"
+	["logger.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/logger.lua",
+	["internetHandler.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/internetHandler.lua",
 }
 
 return fileManifest
