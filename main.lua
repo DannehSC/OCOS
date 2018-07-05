e28593eea7d3a605361916ddcaad0570972003d7
DannehSC <dangerspookycanyon@gmail.com>
2018-07-04 19:58:06 -0500

    file update

--- main.lua
+++ main.lua
@@ -1,21 +1,34 @@
 local gui = require('GUI')
 local color = require('color')
 local component = require('component')
+local term = require('term')
+
+local RGB = color.RGBToInteger
 
 local mainContainer = gui.fullScreenContainer()
-local RGB = color.RGBToInteger
-local bar = gui.progressBar(160 / 2 - 40 , 20, 80, RGB(255, 255, 255), RGB(0, 0, 0), RGB(0, 0, 255), 0)
 
-mainContainer:addChild(bar)
+mainContainer:addChild(gui.panel(1, 1, mainContainer.width, mainContainer.height, 0x2D2D2D))
+
+local bar = mainContainer:addChild(gui.progressBar((mainContainer.width / 2) - 40 , mainContainer.height - 5, 80, 0x3366CC, 0xEEEEEE, 0xEEEEEE, 0, true, true, "Percentage: "))
 
 local percent = 100 * getTotalMemory() / ( getTotalMemory() + getUsedMemory() )
 
-print('Initializing... [Mem Used: ' .. percent .. ']')
+term.clear()
+mainContainer:drawOnScreen()
 
-mainContainer:drawOnScreen(true)
-mainContainer:startEventHandling()
-
-for i = 1, 5 do
-	local perc = 100 * 5 / (5 + i)
-	bar.value = perc
-end
+coroutine.wrap(function()
+	local perc = 0
+	for i = 1, 5 do
+		perc = perc + (100 / 5)
+		os.sleep(1)
+		bar.value = perc
+		mainContainer:drawOnScreen()
+		if i == 5 then
+			os.sleep(2)
+			term.clear()
+			mainContainer:addChild(gui.panel(1, 1, mainContainer.width, mainContainer.height, 0x00000000))
+			mainContainer:drawOnScreen()
+			os.exit()
+		end
+	end
+end)()
