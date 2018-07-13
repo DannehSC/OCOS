dd4e7ecf7e61d6cdeddc3f9a9d1a0e835d86d982
DannehSC <dangerspookycanyon@gmail.com>
2018-07-12 21:01:59 -0500

    Update downloadmanifest.lua

--- downloadmanifest.lua
+++ downloadmanifest.lua
@@ -2,13 +2,13 @@
 
 local fileManifest = {
 	--Boot
-	["/OCOS/boot/kernel.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/kernel.lua",
-	["/OCOS/boot/main.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/main.lua",
+	["/OCOS/boot/kernel.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/OCOS/boot/kernel.lua",
+	["/OCOS/boot/main.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/OCOS/boot/main.lua",
 	--Lib
-	["/OCOS/lib/authentication.lua"] = "",
-	["/OCOS/lib/internetHandler.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/internetHandler.lua",
-	["/OCOS/lib/logger.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/logger.lua",
-	["/OCOS/lib/sha256.lua"] = "",
+	["/OCOS/lib/authentication.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/OCOS/lib/authentication.lua",
+	["/OCOS/lib/internetHandler.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/OCOS/lib/internetHandler.lua",
+	["/OCOS/lib/logger.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/OCOS/lib/logger.lua",
+	["/OCOS/lib/sha256.lua"] = "https://raw.githubusercontent.com/DannehSC/OCOS/master/OCOS/lib/sha256.lua",
 }
 
 return fileManifest
